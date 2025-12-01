397ff98c1e5262cfd063d418c57b9a6f1b0a5164
Victor-Picallo <victor.p9646@gmail.com>
2025-12-01 14:27:28 +0100

    segundo dia

--- src/pages/Ejem21.tsx
+++ src/pages/Ejem21.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 
+// https://localhost:5000/cursos/234
 export default function Ejem21() {
 
     const { id } = useParams()
--- src/pages/Ejem22.tsx
+++ src/pages/Ejem22.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useState, type FormEvent } from "react"
+import { useSearchParams } from "react-router-dom"
+
+const names = ['Ana', 'Juan', 'Luis', 'Antonio', 'Pedro']
+
 export default function Ejem22(){
-    return <div></div>
+
+    const [searchParam, setSearchParam] = useSearchParams()
+    const qFromUrl = searchParam.get('q') ?? ''
+
+    const [search, setSearch] = useState(qFromUrl)
+    const [nameFilter, setNameFilter] = useState<string[]>(names)
+
+
+    useEffect (() => {
+        setSearchParam(qFromUrl)
+    },[qFromUrl])
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>){
+        e.preventDefault()
+
+        
+        setNameFilter( names.filter( name => name.toLowerCase().startsWith(qFromUrl.trim().toLowerCase()) ))
+        setSearchParam ({ q: search } )
+    }
+
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <input value={search} onChange={(e)=>setSearch(e.target.value)} placeholder="Buscar por nombre..."/>
+            <button type="submit">Buscar</button>
+        </form>
+        <ul>
+            {nameFilter.map( (name, index) => <li key={index}>{name}</li>)}
+        </ul>
+    </div>
 }
